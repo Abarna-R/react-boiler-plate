115cddc4488f2f92c6084bf519a3956e29df2283
Abarna-R <abarna.r21@gmail.com>
2019-03-11 12:22:43 +0530

    new

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// const hello=React.createElement('h1',null,'hello world,ReactJs');
+// ReactDOM.render(hello,document.getElementById('root'));
+const domContainer = document.querySelector('#like_button_container');
+ReactDOM.render(e(LikeButton), domContainer);
+// ReactDOM.render(React.createElement('h1',null,'hello,webpack',document.getElementsById('root')));
